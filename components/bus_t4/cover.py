a3753290c50c1918dcb01e0c628377da68e674bd
xCapeITA <xcape.ita@gmail.com>
2026-07-02 23:55:46 +0200

    Update cover.py

--- components/bus_t4/cover.py
+++ components/bus_t4/cover.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import cover
 from esphome.const import CONF_ADDRESS, CONF_ID, CONF_USE_ADDRESS
 
-# Definizione del namespace e della classe C++ associata al template lupanosix_blu
+# Definizione del namespace e aggancio classe lupanosix_blu
 bus_t4_ns = cg.esphome_ns.namespace('bus_t4')
 NiceBusT4 = bus_t4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
 
@@ -23,7 +23,7 @@ def to_code(config):
     yield cg.register_component(var, config)
     yield cover.register_cover(var, config)
 
-    # Scomposizione dell'indirizzo di destinazione (addr_to) nei due byte costituenti
+    # Gestione dell'indirizzo destinazione nell'array di byte nativo C++
     if CONF_ADDRESS in config:
         address = config[CONF_ADDRESS]
         addr_hi = (address >> 8) & 0xFF
@@ -31,7 +31,7 @@ def to_code(config):
         cg.add(var.addr_to[0], addr_hi)
         cg.add(var.addr_to[1], addr_lo)
 
-    # Scomposizione dell'indirizzo sorgente del gateway (addr_from) nei due byte costituenti
+    # Gestione dell'indirizzo sorgente del gateway nell'array di byte nativo C++
     if CONF_USE_ADDRESS in config:
         use_address = config[CONF_USE_ADDRESS]
         from_hi = (use_address >> 8) & 0xFF
